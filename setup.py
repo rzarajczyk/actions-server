d5e3970f8ae0beedfb5fb10142702f7ea21779a4
Rafal Zarajczyk <rzarajczyk@box.com>
2022-07-28 22:48:25 +0200

    requirement fix

--- setup.py
+++ setup.py
@@ -25,5 +25,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["requests>=2.27.1"],
+    install_requires=["requests>=2.27.1", "multipart>=0.2.4"],
 )
