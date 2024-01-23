c29073433006f6c39357019d55117a75098fc006
rzarajczyk <rzarajczyk@gmail.com>
2024-01-23 23:53:39 +0100

    v0.0.10

--- setup.py
+++ setup.py
@@ -27,5 +27,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["requests>=2.31.0", "multipart>=0.2.4", "deser>=0.0.1.dev1706048327"],
+    install_requires=["requests>=2.31.0", "multipart>=0.2.4", "deser>=0.0.1"],
 )
